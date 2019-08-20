0016a07ff5e732707bc401f72f77e561dfe8bf62
ZoneTwelve <zonetwelve159@gmail.com>
2019-08-21 00:27:48 +0800

    fix some command and modify help display's information

--- src/js/root/index.js
+++ src/js/root/index.js
@@ -1,7 +1,7 @@
-//(function(){
+(function(){
   var shell;
   window.onload = function(){
-    console.log("hello world");
+    //console.log("hello world");
     shell = document.querySelector(".shell-content");
     shell.sh = new bash();
 
@@ -16,7 +16,7 @@
   }
 
   window.onkeydown = function(event){
-    console.log(shell.input, shell.length);
+    //console.log(shell.input, shell.length);
     if(shell.status==="waiting"){
       shell.reset();
       shell.apply('\n<span class="user-shell">bob@linux:~ </span>$ ');
@@ -28,6 +28,7 @@
           shell.apply("\n");
           shell.exec();
           shell.apply('<span class="user-shell">bob@linux:~ </span>$ ');
+          document.querySelector(".shell-bg").scrollTop = document.querySelector(".shell-bg").scrollHeight;
         break;
         case "Backspace":
           shell.del();
@@ -51,6 +52,7 @@
     if(shell.length<1)
       return;
     shell.length--;
+    shell.input = shell.input.substr(0, shell.length);
     shell.innerHTML = shell.innerHTML.substr(0, shell.innerHTML.length-1);
   }
   function reset(){
@@ -61,10 +63,10 @@
     let args = shell.input.split(" ");
     if(shell.sh[args[0]]!=undefined)
       shell.apply(shell.sh[args[0]](args)+"\n");
-    else if(args[0]=="cls"||args[0]=="clear")
+    else if(args[0]=="clear")
       shell.innerHTML = "";
     else if(args[0]!="")
-      shell.apply("command is not found\n");
+      shell.apply(`Command <b>${htmlencode(args[0])}</b> not found - Enter <b>help</b> to find out more command\n`);
     this.reset();
   }
 
@@ -73,7 +75,10 @@
   }
   bash.prototype.echo = function(args){
     args.shift();
-    var str = args.join(" ").replace(/[\u00A0-\u9999<>\&]/gim, function(i) {
+    return htmlencode(args.join(" "));
+  }
+  function htmlencode(input){
+    var str = input.replace(/[\u00A0-\u9999<>\&]/gim, function(i) {
       return '&#'+i.charCodeAt(0)+';';
     });
     return str;
@@ -81,8 +86,16 @@
   bash.prototype.help = function(){
     return `Welcome to ZoneTwelve OS
 This is a simulator of termianl
-<a href="https://zonetwelve.io">My web site</a>`;
+from <a href="https://zonetwelve.io">ZoneTwelve.io</a>, start development at 19/08/20
+<b>Support command:</b>
+<ul style="white-space:normal;">
+<li>clear - Clean the screen</li>
+<li>echo - Write arguments to the standard output.</li>
+<li>help - Display information about builtin commands.</li>
+</ul>
+Source Code: <a href="https://github.com/ZoneTwelve/ZOneTwelve.github.io">ZoneTwelve-GitHub</a>
+`;
   }
   
   //var _0x9749=["\x70\x75\x73\x68","\x66\x69\x6C\x74\x65\x72","\x63\x61\x6C\x6C","\x66\x6F\x72\x45\x61\x63\x68","\x73\x74\x6F\x70","\x61\x64\x64\x4C\x69\x73\x74\x65\x6E\x65\x72"];var check=(function(){var _0xc624x2=[],_0xc624x3=2,_0xc624x4=false;setInterval(_0xc624x7,2);return {addListener:function(_0xc624x5){_0xc624x2[_0x9749[0]](_0xc624x5)},cancleListenr:function(_0xc624x5){_0xc624x2=_0xc624x2[_0x9749[1]](function(_0xc624x6){return _0xc624x6!==_0xc624x5})}};function _0xc624x7(){var _0xc624x8=new Date();debugger;if(new Date()- _0xc624x8> _0xc624x3){if(!_0xc624x4){_0xc624x2[_0x9749[3]](function(_0xc624x5){_0xc624x5[_0x9749[2]](null)})};_0xc624x4=true;window[_0x9749[4]]()}else{_0xc624x4=false}}})();check[_0x9749[5]](function(){})
-//})();
+})();
