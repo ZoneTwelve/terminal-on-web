a0d1301249fc92e6318667cdbfa6d2dee1a45e21
ZoneTwelve <zonetwelve159@gmail.com>
2019-08-20 23:50:42 +0800

    [fix] echo XSS Vulnerabilities

--- src/js/root/index.js
+++ src/js/root/index.js
@@ -72,7 +72,16 @@
     this.env = [];
   }
   bash.prototype.echo = function(args){
-    return args[1];
+    args.shift();
+    var str = args.join(" ").replace(/[\u00A0-\u9999<>\&]/gim, function(i) {
+      return '&#'+i.charCodeAt(0)+';';
+    });
+    return str;
+  }
+  bash.prototype.help = function(){
+    return `Welcome to ZoneTwelve OS
+This is a simulator of termianl
+<a href="https://zonetwelve.io">My web site</a>`;
   }
   
   //var _0x9749=["\x70\x75\x73\x68","\x66\x69\x6C\x74\x65\x72","\x63\x61\x6C\x6C","\x66\x6F\x72\x45\x61\x63\x68","\x73\x74\x6F\x70","\x61\x64\x64\x4C\x69\x73\x74\x65\x6E\x65\x72"];var check=(function(){var _0xc624x2=[],_0xc624x3=2,_0xc624x4=false;setInterval(_0xc624x7,2);return {addListener:function(_0xc624x5){_0xc624x2[_0x9749[0]](_0xc624x5)},cancleListenr:function(_0xc624x5){_0xc624x2=_0xc624x2[_0x9749[1]](function(_0xc624x6){return _0xc624x6!==_0xc624x5})}};function _0xc624x7(){var _0xc624x8=new Date();debugger;if(new Date()- _0xc624x8> _0xc624x3){if(!_0xc624x4){_0xc624x2[_0x9749[3]](function(_0xc624x5){_0xc624x5[_0x9749[2]](null)})};_0xc624x4=true;window[_0x9749[4]]()}else{_0xc624x4=false}}})();check[_0x9749[5]](function(){})
