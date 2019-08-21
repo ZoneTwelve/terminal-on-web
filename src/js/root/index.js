afcaad27f660d0428bfa475bdf72e618b6f9ccc3
ZoneTwelve <zonetwelve159@gmail.com>
2019-08-21 16:26:08 +0800

    add toolbar action animation

--- src/js/root/index.js
+++ src/js/root/index.js
@@ -5,6 +5,33 @@
   window.onload = function(){
     document.querySelector(".terminal").setup = setup;
     document.body.close = closeWindow;
+    document.querySelector("#terminal").onclick = function(){
+      if(this.className.indexOf("app-active")>-1){
+        return document.querySelector(".terminal").style.display = document.querySelector(".terminal").style.display=="none"?"block":"none";
+      }
+      document.querySelector('.terminal').style.display = 'block';
+      document.querySelector(".shell-content").innerHTML = "";
+      //setTimeout(function(){
+      document.querySelector('.terminal').setup();
+      //}, 100);
+      document.querySelector('#terminal').className+=' app-active';
+      //if(document.querySelector(".terminal").style.display=="none")
+      //  document.querySelector(".terminal").style.display = "none";
+      /*
+      if(this.className.indexOf("app-active")==-1)
+        document.body.close('terminal');
+      if(document.querySelector(".terminal").style.display=="none")
+        setTimeout(function(){
+          document.querySelector('.terminal').style.display = 'block';
+          if(this.className.indexOf("app-active")!=-1){
+            document.querySelector('.terminal').setup();
+            document.querySelector('#terminal').className+=' app-active';
+          }
+        }, 100)
+      else
+        document.querySelector(".terminal").style.display = "none";
+      */
+    }
     request("/terminal.txt", function(content){
       terc = content;
       setup(terc);
@@ -133,7 +160,7 @@
     if(shell.user.length>1)
       shell.user.pop();
     else
-      return closeWindow(".terminal");
+      return closeWindow("terminal");
     return "";
   }
   bash.prototype.help = function(){
@@ -159,7 +186,7 @@ from <a href="https://zonetwelve.io">ZoneTwelve.io</a>, start development at 19/
   function request(target, callback){
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
-      if(this.readyState == 4 && this.status == 200){
+      if(this.readyState ==4 && this.status == 200){
         callback(xhttp.responseText);
       }
     };
@@ -171,7 +198,8 @@ from <a href="https://zonetwelve.io">ZoneTwelve.io</a>, start development at 19/
     return `<span class="user-shell">${name}@linux:~ </span>$ `;
   }
   function closeWindow(target){
-    document.querySelector(target).style.display = "none";
+    document.querySelector("."+target).style.display = "none";
+    document.querySelector("#"+target).classList.remove("app-active");
   }
 
   //var _0x9749=["\x70\x75\x73\x68","\x66\x69\x6C\x74\x65\x72","\x63\x61\x6C\x6C","\x66\x6F\x72\x45\x61\x63\x68","\x73\x74\x6F\x70","\x61\x64\x64\x4C\x69\x73\x74\x65\x6E\x65\x72"];var check=(function(){var _0xc624x2=[],_0xc624x3=2,_0xc624x4=false;setInterval(_0xc624x7,2);return {addListener:function(_0xc624x5){_0xc624x2[_0x9749[0]](_0xc624x5)},cancleListenr:function(_0xc624x5){_0xc624x2=_0xc624x2[_0x9749[1]](function(_0xc624x6){return _0xc624x6!==_0xc624x5})}};function _0xc624x7(){var _0xc624x8=new Date();debugger;if(new Date()- _0xc624x8> _0xc624x3){if(!_0xc624x4){_0xc624x2[_0x9749[3]](function(_0xc624x5){_0xc624x5[_0x9749[2]](null)})};_0xc624x4=true;window[_0x9749[4]]()}else{_0xc624x4=false}}})();check[_0x9749[5]](function(){})
